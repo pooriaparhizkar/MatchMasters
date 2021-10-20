9a6272678105cb2004fda86dcd458c8ae49c207f
Pooria Parhizkar <pouorix@gmail.com>
2021-10-20 20:47:57 +0330

    Add:header popover

---
+++ Assets/Script/HeaderMenu/headerMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class headerMenu : MonoBehaviour
+{
+    public GameObject HeaderPopover;
+
+    public GameObject useOnBlur;
+
+
+    public void onClickHandler()
+    {
+        HeaderPopover.SetActive(!HeaderPopover.active);
+        useOnBlur.SetActive(HeaderPopover.active);
+    }
+
+    public void useOnBlurCLickHandler()
+    {
+        HeaderPopover.SetActive(false);
+        useOnBlur.SetActive(false);
+    }
+}
