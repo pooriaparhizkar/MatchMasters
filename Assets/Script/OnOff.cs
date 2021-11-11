7b0454802b3a7375903f0c34ab264dd21016d6ca
mohammadreza <mrezamaghouli@gmail.com>
2021-11-11 13:29:24 +0330

    add setting menu

---
+++ Assets/Script/OnOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOff : MonoBehaviour
+{
+    public GameObject onooffmodal;
+
+    public GameObject useOnBlur;
+
+
+    public void Onoff()
+    {
+        onooffmodal.SetActive(!onooffmodal.active);
+        useOnBlur.SetActive(onooffmodal.active);
+    }
+
+    public void useOnBlurCLickHandler()
+    {
+        Settingmodal.SetActive(false);
+        useOnBlur.SetActive(false);
+    }
+}
