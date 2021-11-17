db112f7ff16e323af12537e26cca4011a6135862
mohammadreza <mrezamaghouli@gmail.com>
2021-11-17 14:47:03 +0330

    Add:profile layout

---
+++ Assets/Script/ProfilePopUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfilePopUp : MonoBehaviour
+{
+    public GameObject Profilemodal;
+
+    public GameObject useOnBlur;
+
+
+    public void Profilepopup()
+    {
+        Profilemodal.SetActive(!Profilemodal.active);
+        useOnBlur.SetActive(Profilemodal.active);
+    }
+
+    public void useOnBlurCLickHandler()
+    {
+        Profilemodal.SetActive(false);
+        useOnBlur.SetActive(false);
+    }
+}
