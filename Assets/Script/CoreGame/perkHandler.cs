9f425c2a26d3764a9627be127265d9ea3476390c
Pooria Parhizkar <pouorix@gmail.com>
2022-01-22 14:51:13 +0330

    add:perk layouts

---
+++ Assets/Script/CoreGame/perkHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perkHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void hammerPerkClick()
+    {
+        Debug.Log("perk clicked");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
