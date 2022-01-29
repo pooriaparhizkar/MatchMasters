e75a7929792d3d66894d8ce5b5e5d3e5444a1288
mohammadreza <mrezamaghouli@gmail.com>
2022-01-29 15:36:53 +0330

    milestonesizefixing

---
+++ Assets/Script/MainPage/loadmilestone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadmilestone : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+        public void loadmilstone2()
+        {
+            SceneManager.LoadScene("milestone");
+        }
+        
+}
